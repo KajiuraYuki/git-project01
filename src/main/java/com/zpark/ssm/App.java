ef47fd144aaba51c5e548a8723a35e262bd62420
Kajiura Yuki <3061740475@qq.com>
2023-05-06 09:09:58 +0800

    xyh hotfix first commit

--- src/main/java/com/zpark/ssm/App.java
+++ src/main/java/com/zpark/ssm/App.java
@@ -4,5 +4,6 @@ public class App {
     public static void main(String[] args) {
         System.out.println("Hello Git!");
         System.out.println("Hello Git! xyh master second commit");
+        System.out.println("Hello Git! xyh hotfix first commit");
     }
 }
